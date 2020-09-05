796d7885bfa14c12076fe90c12b8cc9c7f7d2ae5
sandeepkokane <kokane.sandeep97@gmail.com>
2020-09-05 14:33:20 +0530

    config file added

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,10 +1,17 @@
 package com.spring.jdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.jdbc.dao.ProductDao;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Spring JDBC Project -> CRUD Operations" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        ProductDao productDao = context.getBean("productDao",ProductDao.class);
+        
     }
 }
